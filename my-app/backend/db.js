9919353994ff34813240147af86f7d28e67e9d68
Abhilash <abhilashbangera97@gmail.com>
2025-04-15 14:52:51 +0530

    Setup Schema & models

---
+++ my-app/backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const mongoURI =
+  "mongodb+srv://user:inotebook@inotebook.kevw0wt.mongodb.net/?retryWrites=true&w=majority&appName=iNotebook";
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("✅ Connected to Mongo successfully!!");
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:", error.message);
+    process.exit(1); // optional: kill app if DB isn't connected
+  }
+};
+
+module.exports = connectToMongo;
